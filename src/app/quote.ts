95ea880bfb47594a98343d17145b00bbdce9a312
Melvin Omega <melvinomega151@gmail.com>
2021-08-22 18:16:37 +0300

    add date section

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,21 +1,6 @@
 export class Quote {
-    showDescription: boolean;
-    constructor(public id: number, public name: string, public description: string) {
+    public showDescription: boolean;
+    constructor(public id: number, public name: string, public description: string, public completeDate: Date) {
         this.showDescription=false;
     }
 }
-
-
-
-// export class Quote {
-//     id: number;
-//     name: string;
-//     description: string;
-
-
-//     constructor(id:number, name:string, description:string){
-//         this.id=id;
-//         this.name=name;
-//         this.description=description
-//     }
-// }
