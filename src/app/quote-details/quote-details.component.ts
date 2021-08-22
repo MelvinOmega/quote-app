851e3ad883958a81ec30b5ba29a51895a18f1f29
Melvin Omega <melvinomega151@gmail.com>
2021-08-22 15:26:24 +0300

    modify detail.html

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,3 +1,16 @@
 import { from } from "rxjs";
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { }
+import { Quote } from "@angular/compiler";
+
+export class QuoteDetailComponent implements OnInit {
+    @Input() quotes: Quote;
+    @Output() isComplete = new EventEmitter<boolean>();
+
+    quoteComplete(complete: boolean) {
+        this.isComplete.emit(complete);
+    }
+    constructor() {}
+    ngOnInit() {
+        
+    }
+}
