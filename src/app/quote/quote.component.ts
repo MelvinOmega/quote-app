26dcf407d902b6bcc92cca6f772f0337d8b2d945
Melvin Omega <melvinomega151@gmail.com>
2021-08-23 16:45:47 +0300

    add background color

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,12 +8,12 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
-    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son' ,new Date(2020,3,14)),
-    new Quote(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2020,3,14)),
-    new Quote(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2020,3,14)),
-    new Quote(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2020,3,14)),
-    new Quote(5,'Solve math homework','Damn Math',new Date(2020,3,14)),
-    new Quote(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2020,3,14)),
+    new Quote(1, 'Life is short and we all have to do so many things. we need a constant source of motivation which pushes us when we are tired', ' by Campbell' ,new Date(2020,3,14)),
+    new Quote(2,'Life is a question and how we live it is our answer. See your life as a question and live your journey of life such that it becomes the best answer to that question.','Gary Keller',new Date(2020,3,14)),
+    new Quote(3,' Short life with huge respect is much better than a normal long life without respect.Try to live that kind of life which gives you huge respect and popularity. So that people will remember you even after your death. Most people nowadays live life in a comfort zone and even die in it.','Zainaba Jamal',new Date(2020,3,14)),
+    new Quote(4,'Life is a precious gift given to us by nature. It is already very beautiful. But we forget this thing due to some bad experiences','Jimmy Craig',new Date(2020,3,14)),
+    new Quote(5,'Life can be shaped in the desired way by changing your habits.We can shape our future in a better way changing our weak lifestyle into a healthier and lively one. It can be only done by replacing bad habits with good ones.','Justin Martin',new Date(2020,3,14)),
+    new Quote(6,'Life is given to us by God to take this humanity to whole new level.','Marcus Garvey',new Date(2020,3,14)),
   ];
 
   toggleDetails(index: any){
